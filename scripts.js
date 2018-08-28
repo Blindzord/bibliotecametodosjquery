38ec4eaab1d7fe23f77b188f46970a70f8b300fd
Oscar Jimenez <42780756+Blindzord@users.noreply.github.com>
2018-08-28 13:07:53 -0600

    Add files via upload
    Scripts de Iniciacion en jQuery

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+    'use strict';
+$('div.logo img').on('click', function(){
+  $(this).remove();
+});
+
+
+
+/*
+    $('.logo img').on('click', function() {
+        console.log("has hecho click en el logo");
+    });
+*/
+
+
+
+})
